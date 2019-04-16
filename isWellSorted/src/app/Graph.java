a6dea12d5cd0c6717d1cb66e82cdc18ec8598c47
aali2s <40173164+aali2s@users.noreply.github.com>
2019-04-17 01:27:55 +0200

    Graph implementing isWellSortedIF

---
+++ isWellSorted/src/app/Graph.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.*;
+
+public class Graph implements SortedIF{
+
+	public Graph(String[][] array) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isWellSorted(String[] sequence) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
