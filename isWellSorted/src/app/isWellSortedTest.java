71b24c34988918f1c0c99bcaf94281668c39b235
aali2s <40173164+aali2s@users.noreply.github.com>
2019-04-17 01:18:24 +0200

    2nd one

---
+++ isWellSorted/src/app/isWellSortedTest.java
@@ -0,0 +1,26 @@
+package app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+public class isWellSortedTest {
+	SortedIF g;
+	
+	@Before
+	public void setUp() {
+		String[][] array = { { "5", "0" }, { "4", "0" }, { "5", "2" }, { "4", "1" }, { "3", "1" }, { "2", "3" }};
+		//String[][] array = { { "A", "C" }, { "C", "D" }, { "B", "C" }};
+		//String[][] array = { { "A", "B" }, { "B", "C" }};
+		g=new Graph(array);
+	}
+	
+
+	@Test
+	public void testIsWellSorted() {
+		String[] sequence = { "5", "4", "0", "3", "1" };
+		//String[] sequence = { "B","C","B","A" };
+		//String[] sequence = { "C","A" };
+		assertTrue(g.isWellSorted(sequence));
+	}
+}
